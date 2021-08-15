fcb6e2abfd0dc990c5418b430b81e5c75cf5390f
oldbelom <oldbelom@gmail.com>
2021-08-15 16:50:40 +0300

    initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+        code: 100,
+      },
+    ],
+  },
+  plugins: ["jest"],
+};
