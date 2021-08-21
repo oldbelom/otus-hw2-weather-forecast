e60fa877b6ed17836eebb4d1894a88f16c95e329
oldbelom <oldbelom@gmail.com>
2021-08-21 20:36:26 +0300

    installed and configured webpack

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "current" } }]],
-};
+module.exports = (api) => ({
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: api.caller((caller) => caller && caller.target === "node")
+          ? { node: "current" }
+          : { chrome: "58", ie: "11" },
+      },
+    ],
+  ],
+  plugins: ["@babel/plugin-transform-runtime"],
+});
